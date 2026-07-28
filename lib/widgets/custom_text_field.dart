cd4902d4b910381217c2db16a89ebb0347433061
Lokesh Parse <lokeshparse01@gmail.com>
2026-07-28 14:56:52 +0530

    feat: add reusable UI components

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../core/constants/app_colors.dart';
+import '../core/constants/app_radius.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        border: OutlineInputBorder(
+          borderRadius: AppRadius.medium,
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: AppRadius.medium,
+          borderSide: const BorderSide(
+            color: AppColors.primary,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
